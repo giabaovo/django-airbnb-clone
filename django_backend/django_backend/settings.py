e16af4807eaf636d038ab950260c90c8cfd2da21
Bao Vo <giabaovo123456@gmail.com>
2024-08-23 20:25:52 +0700

    [User Auth] Implement login register logout APIs

--- django_backend/django_backend/settings.py
+++ django_backend/django_backend/settings.py
@@ -49,7 +49,7 @@ REST_FRAMEWORK = {
 
 CORS_ALLOWED_ORIGINS = [
     'http://127.0.0.1:8000',
-    'http://127.0.0.1:3000',
+    'http://localhost:3000',
 ]
 
 REST_AUTH = {
@@ -73,6 +73,7 @@ INSTALLED_APPS = [
 
     'allauth',
     'allauth.account',
+    'allauth.socialaccount',
 
     'dj_rest_auth',
     'dj_rest_auth.registration',
